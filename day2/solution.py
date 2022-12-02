f4c5572e3a2e06573f70c939b902eb34899180e1
Miha Muskinja <miha.muskinja@cern.ch>
2022-12-01 22:22:17 -0800

    nicer day2

--- day2/solution.py
+++ day2/solution.py
@@ -1,71 +1,29 @@
 #!/usr/bin/python3
-
 import os
-
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-f = open(os.path.join(__location__, 'input'))
 
-score = 0
-for l in f:
-    o = l.split()[0]
-    me = l.split()[1]
-    print(o, me)
-    if me == "X":
-        score += 1
-    if me == "Y":
-        score += 2
-    if me == "Z":
-        score += 3
+X = [l.strip() for l in open(os.path.join(__location__, 'input'))]
+p1 = 0
+p2 = 0
 
-    if me == "X" and o == "A":
-        score += 3
-    elif me == "Y" and o == "B":
-        score += 3
-    elif me == "Z" and o == "C":
-        score += 3
-    elif me == "X" and o == "C":
-        score += 6
-    elif me == "Y" and o == "A":
-        score += 6
-    elif me == "Z" and o == "B":
-        score += 6
+for x in X:
+    o, m = x.split()
+    o = ord(o) - ord("A")
+    m = ord(m) - ord("X")
 
+    p1 += m + 1
+    r = (o - m) % 3
+    if r == 0:
+        p1 += 3
+    elif r == 2:
+        p1 += 6
 
-print(f"solution a) {score}")
-f.close()
+    if m == 1:
+        p2 += 3 + o + 1
+    elif m == 0:
+        p2 += (o + 2) % 3  + 1
+    else:
+        p2 += 6 + (o + 1) % 3 + 1
 
-f = open(os.path.join(__location__, 'input'))
-
-score = 0
-for l in f:
-    o = l.split()[0]
-    me = l.split()[1]
-    print(o, me)
-    if me == "X":
-        if o == "A":
-            score += 3
-        elif o == "B":
-            score += 1
-        elif o == "C":
-            score += 2
-    elif me == "Y":
-        score += 3
-        if o == "A":
-            score += 1
-        elif o == "B":
-            score += 2
-        elif o == "C":
-            score += 3
-    elif me == "Z":
-        score += 6
-        if o == "A":
-            score += 2
-        elif o == "B":
-            score += 3
-        elif o == "C":
-            score += 1
-
-
-print(f"solution b) {score}")
-
-f.close()
+print(p1)
+print(p2)
